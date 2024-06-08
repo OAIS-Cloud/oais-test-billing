6760e86c572d9d1f46414eb6d698e36e79e3e3df
David <notdavidzin@gmail.com>
2024-06-08 19:02:48 -0300

    feat(ui): add custom 'oais-blue' theme

--- web/src/layouts/default.vue
+++ web/src/layouts/default.vue
@@ -1,5 +1,10 @@
+<script lang="ts" setup>
+const { toggleTheme } = useTheme();
+</script>
+
 <template>
   <div>
+    <UButton @click="toggleTheme" size="sm" square icon="i-heroicons-sun" variant="solid" />
     <slot />
   </div>
 </template>
--- web/tailwind.config.ts
+++ web/tailwind.config.ts
@@ -7,6 +7,21 @@ export default <Partial<Config>>{
       fontFamily: {
         sans: 'Inter, sans-serif',
       },
+      colors: {
+        'oais-blue': {
+          50: '#eff6ff',
+          100: '#dae9ff',
+          200: '#bddaff',
+          300: '#90c3ff',
+          400: '#58a0fe',
+          500: '#367efb',
+          600: '#205ef0',
+          700: '#1849dd',
+          800: '#1a3cb3',
+          900: '#1b378d',
+          950: '#152356',
+        },
+      },
     },
   },
 };
