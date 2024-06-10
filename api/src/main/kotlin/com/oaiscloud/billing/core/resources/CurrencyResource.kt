b75d217ff61b6c7809a7b00ff659781e87ed3772
David <notdavidzin@gmail.com>
2024-06-10 16:53:42 -0300

    feat(CurrencyResource): return the updated currency after update

--- api/src/main/kotlin/com/oaiscloud/billing/core/resources/CurrencyResource.kt
+++ api/src/main/kotlin/com/oaiscloud/billing/core/resources/CurrencyResource.kt
@@ -74,8 +74,8 @@ class CurrencyResource(private val currencyService: CurrencyService) {
             name = updateCurrencyDTO.name.trim();
         };
 
-        currencyService.updateCurrencyById(id, currency);
+        val updatedCurrency = currencyService.updateCurrencyById(id, currency);
 
-        return Response.ok().build();
+        return Response.status(Response.Status.OK).entity(updatedCurrency).build();
     }
 }
